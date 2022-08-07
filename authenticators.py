41bfce4dce3f58e1a1aea25cf784c20fb8632014
Sandro-Meireles <playploygames@gmail.com>
2022-08-07 20:39:09 -0300

    feat: Add OIDC autenticators and remove clients

---
+++ authenticators.py
@@ -0,0 +1,56 @@
+from abc import ABCMeta, abstractmethod
+
+import requests
+
+import settings
+
+
+class OIDCAuth(metaclass=ABCMeta):
+    def _request(self, data: dict) -> "Response":
+        response = requests.post(url=settings.OIDC_OP_TOKEN_ENDPOINT, data=data)
+        response.raise_for_status()
+
+        return response
+
+    @abstractmethod
+    def _get_auth_token(self) -> str:
+        pass
+
+    @property
+    def headers(self) -> dict:
+        return {
+            "Content-Type": "application/json; charset: utf-8",
+            "Authorization": self._get_auth_token(),
+        }
+
+
+class OIDCPasswordAuth(OIDCAuth):
+    def _get_auth_token(self) -> str:
+        data = dict(
+            client_id=settings.PROJECT_CLIENT_ID,
+            username=settings.PROJECT_ADMIN_USER_EMAIL,
+            password=settings.PROJECT_ADMIN_USER_PASSWORD,
+            grant_type="password",
+        )
+
+        response = self._request(data)
+        token = response.json().get("access_token")
+
+        return f"Bearer {token}"
+
+
+class OIDCClientCredentialsAuth(OIDCAuth):
+    def __init__(self, settings_class) -> None:
+        self.settings = settings_class
+
+    def _get_auth_token(self) -> str:
+        data = dict(
+            client_id=self.settings.OIDC_RP_CLIENT_ID,
+            client_secret=self.settings.OIDC_RP_CLIENT_SECRET,
+            grant_type="client_credentials",
+        )
+
+        response = self._request(data)
+        token = response.json().get("access_token")
+
+        return f"Bearer {token}"
